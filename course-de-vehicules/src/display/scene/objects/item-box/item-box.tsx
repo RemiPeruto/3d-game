f210aa7824ebd9667861ecd2af60b9e95dcba1f2
RemiP <remipe@theodo.fr>
2023-03-29 19:05:47 +0200

    rotating item box

--- course-de-vehicules/src/display/scene/objects/item-box/item-box.tsx
+++ course-de-vehicules/src/display/scene/objects/item-box/item-box.tsx
@@ -1,17 +1,25 @@
 import { Euler, Vector3 } from "three";
-import React, { useEffect } from "react";
-import { useLoader } from "@react-three/fiber";
+import React, { useEffect, useState } from "react";
+import { useFrame, useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const ItemBox = () => {
-  const itemBox = "assets/mario_kart_item_box/scene.gltf";
+  const itemBox = "assets/mario_kart_item_box/scene.gltf"; //https://sketchfab.com/3d-models/mario-kart-item-box-8f6a2b6b17b844c5b5dfa38375289975
   const gltf = useLoader(GLTFLoader, itemBox);
+  const [rotation, setRotation] = useState<Euler>(new Euler(0, 0, 0));
   useEffect(() => {
     gltf.scene.scale.multiplyScalar(0.02);
   }, []);
 
+  useFrame((state, delta, frame) => {
+    const dtheta = new Vector3(1, 1, 1)
+      .multiplyScalar(delta)
+      .add(new Vector3(rotation.x, rotation.y, rotation.z));
+    setRotation(new Euler().setFromVector3(dtheta));
+  });
+
   return (
-    <group rotation={new Euler(1, 1, 1)} position={new Vector3(4, 1, 0)}>
+    <group rotation={rotation} position={new Vector3(4, 1, 0)}>
       <primitive object={gltf.scene} position={new Vector3(0, 0, 0)} />
     </group>
   );
