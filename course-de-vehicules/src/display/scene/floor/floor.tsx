48bec1b09a5c2b491e02911f0dd6ae6041a1c006
RemiP <remipe@theodo.fr>
2023-03-31 19:01:33 +0200

    displacement map and normal map

--- course-de-vehicules/src/display/scene/floor/floor.tsx
+++ course-de-vehicules/src/display/scene/floor/floor.tsx
@@ -16,13 +16,26 @@ const Floor = ({
 }: FloorProps) => {
   const rainbowRoad = "assets/floor/rainbow-road.png"; // https://www.spriters-resource.com/fullview/6868/
   const colorMap = useLoader(TextureLoader, rainbowRoad);
+
+  // created heightmap and normalmap following the article -> https://kcclemo.neocities.org/creating-height-and-normal-maps/
+  const rainbowRoadHeightMap = "assets/floor/rainbow-road-bump.png";
+  const heightMap = useLoader(TextureLoader, rainbowRoadHeightMap);
+
+  const rainbowRoadNormalMap = "assets/floor/rainbow-road-bump-normal-map.png";
+  const normalMap = useLoader(TextureLoader, rainbowRoadNormalMap);
+
   return (
     <Plane
       args={[width, height, widthSegments, heightSegments]}
       rotation={[1.5 * Math.PI, 0, 0]}
-      position={[0, 0, 0]}
+      position={[0, -30.5, 0]}
     >
-      <meshStandardMaterial map={colorMap} />
+      <meshStandardMaterial
+        normalMap={normalMap}
+        displacementMap={heightMap}
+        displacementScale={41}
+        map={colorMap}
+      />
     </Plane>
   );
 };
